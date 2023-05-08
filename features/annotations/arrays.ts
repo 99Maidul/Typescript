3b3aa8d138ca88ecb6a011046fc6c2854114123b
CoderNoob2021 <islam.maidul567@gmail.com>
2023-05-08 13:24:19 +0900

    Tuples and arrays

--- features/annotations/arrays.ts
+++ features/annotations/arrays.ts
@@ -1,4 +1,20 @@
-const carMqkers: string[] = ['ford', 'toyota', 'chevy'];
+const carMakers: string[] = ['ford', 'toyota', 'chevy'];
 const dates = [new Date(), new Date()];
 
 const carsByMake: string[][] = [['f150'], ['corolla'], ['camaro']];
+
+const car = carMakers[0];
+const myCar = carMakers.pop();
+
+// Prevent incompatible values
+// carMakers.push(100);
+
+carMakers.map((car: string): string => {
+    return car.toUpperCase();
+}
+);
+// Flexible types
+const importantDates: (Date | string)[] = [new Date()];
+importantDates.push('2030-10-10');
+importantDates.push(new Date());
+// importantDates.push(100); // Error
