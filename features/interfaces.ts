558fc369f7558bc308cd52e00b98265723cd5fd1
CoderNoob2021 <islam.maidul567@gmail.com>
2023-05-09 07:36:46 +0900

    interfave

--- features/interfaces.ts
+++ features/interfaces.ts
@@ -2,4 +2,15 @@ const oldCivic = {
     name: 'civic',
     year : 2000,
     broken: true
+};
+
+interface Vehicle {
+    name: string,
+    year: number,
+    broken: boolean,
+}
+const printVehicle = (vehicle : Vehicle) : void => {
+    console.log(`Name: ${vehicle.name}`);
+    console.log(`Year: ${vehicle.year}`);
+    console.log(`Broken?: ${vehicle.broken}`);
 }
